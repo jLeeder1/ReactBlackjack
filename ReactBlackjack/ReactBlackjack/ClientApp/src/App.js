44e8984f8fd1daa43ba8d3f7c7bba73e97a44fcd
Josh Leeder <joshleeder1@gmail.com>
2020-06-17 15:38:10 +0100

    Deck generation works and console logs currently

--- ReactBlackjack/ReactBlackjack/ClientApp/src/App.js
+++ ReactBlackjack/ReactBlackjack/ClientApp/src/App.js
@@ -4,13 +4,17 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { GenerateDeck } from './Scripts/DeckGeneration/DeckGenerator';
 
 import './custom.css'
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+   
+
+    render() {
+        console.log(GenerateDeck());
     return (
       <Layout>
         <Route exact path='/' component={Home} />
